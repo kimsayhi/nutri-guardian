556970c9a41c0cd7b00fb746b014704b027206fc
kimsayhi <kimse418@gmail.com>
2025-04-13 02:50:25 +0900

    design: 헤더 디자인 변경

--- src/components/layout/GNB/HeaderBar.tsx
+++ src/components/layout/GNB/HeaderBar.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react";
 import Logo from "../../shared/Logo";
+import { IoChatbubbleEllipses } from "react-icons/io5";
 
 export default function HeaderBar({ children }: PropsWithChildren) {
   return (
     <>
-      <header className="fixed top-0 z-20 flex h-[60px] w-full items-center bg-white p-4 md:h-[64px] lg:h-[68px]">
+      <header className="bg-primary fixed top-0 z-20 flex h-[60px] w-full items-center justify-between p-4 md:h-[64px] lg:h-[68px]">
         <Logo />
+        <button className="text-white">
+          <IoChatbubbleEllipses className="text-white" size={24} />
+        </button>
       </header>
       <div className="h-[60px] md:h-[64px] lg:h-[68px]"></div>
       {children}
--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <div className="flex items-center gap-1">
-      <span className="relative bottom-0.5 h-[27px] w-6 md:h-[30px] md:w-7 xl:h-[33px] xl:w-8">
-        <Image src="/images/logo.png" alt="logo" fill className="object-contain" sizes="100%" />
+    <Link href="/main" className="flex items-center gap-1">
+      <span className="full relative bottom-0.5 h-[64] w-8 md:w-9 xl:w-10">
+        <Image
+          src="/images/logo-white.png"
+          alt="logo"
+          fill
+          className="object-contain"
+          sizes="100%"
+        />
       </span>
-      <span className="text-primary-700 font-aggro text-xl font-bold md:text-2xl xl:text-3xl">
+      <span className="font-aggro text-xl font-bold text-white md:text-2xl xl:text-3xl">
         영양지킴이
       </span>
-    </div>
+    </Link>
   );
 }
