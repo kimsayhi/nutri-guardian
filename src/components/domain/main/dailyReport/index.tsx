99b96b86c22f9e1944e6c50714a0c06612d892c2
kimsayhi <kimse418@gmail.com>
2025-04-17 01:42:10 +0900

    fix: 도넛차트 수정

--- src/components/domain/main/dailyReport/index.tsx
+++ src/components/domain/main/dailyReport/index.tsx
@@ -1,105 +1,45 @@
 "use client";
-import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData, ChartOptions } from "chart.js";
+import DonutChart from "@/components/shared/DonutChart";
 import ReportSummary from "./ReportSummary";
 import { FaRegPlusSquare } from "react-icons/fa";
-// Chart.js 필수 컴포넌트 등록
-ChartJS.register(ArcElement, Tooltip, Legend);
 
-interface CustomChartOptions extends ChartOptions<"doughnut"> {
-  cutout?: string;
-}
 export default function DailyReport() {
-  // 탄수화물 차트 데이터 (120%)
-  const carbData: ChartData<"doughnut"> = {
-    labels: ["섭취량", "남은 목표"],
-    datasets: [
-      {
-        data: [120, 0],
-        backgroundColor: ["#f59e0b", "#f3f4f6"],
-        borderWidth: 0,
-      },
-    ],
-  };
-
-  // 단백질 차트 데이터 (37%)
-  const proteinData: ChartData<"doughnut"> = {
-    labels: ["섭취량", "남은 목표"],
-    datasets: [
-      {
-        data: [22, 38],
-        backgroundColor: ["#ef4444", "#f3f4f6"],
-        borderWidth: 0,
-      },
-    ],
-  };
-
-  // 지방 차트 데이터 (93%)
-  const fatData: ChartData<"doughnut"> = {
-    labels: ["섭취량", "남은 목표"],
-    datasets: [
-      {
-        data: [74, 6],
-        backgroundColor: ["#10b981", "#f3f4f6"],
-        borderWidth: 0,
-      },
-    ],
-  };
-
-  // 칼로리 차트 데이터 (105%)
-  const calorieData: ChartData<"doughnut"> = {
-    labels: ["섭취량", "남은 목표"],
-    datasets: [
-      {
-        data: [2300, 0],
-        backgroundColor: ["#10b981", "#f3f4f6"],
-        borderWidth: 0,
-      },
-    ],
-  };
-
-  // 차트 공통 옵션
-  const chartOptions: CustomChartOptions = {
-    cutout: "70%",
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        enabled: false,
-      },
+  // 영양소 데이터
+  const nutritionData = [
+    {
+      name: "탄수화물",
+      value: 120,
+      total: 100,
+      color: "hsl(var(--chart-1))",
+      status: "과다",
+      statusColor: "text-yellow-500",
     },
-    animation: {
-      animateRotate: true,
-      animateScale: true,
+    {
+      name: "단백질",
+      value: 22,
+      total: 60,
+      color: "hsl(var(--chart-2))",
+      status: "매우 부족",
+      statusColor: "text-red-500",
     },
-    responsive: true,
-    maintainAspectRatio: true,
-  };
+    {
+      name: "지방",
+      value: 74,
+      total: 80,
+      color: "hsl(var(--chart-3))",
+      status: "적정",
+      statusColor: "text-green-500",
+    },
+    {
+      name: "총 열량",
+      value: 2300,
+      total: 2200,
+      color: "hsl(var(--chart-4))",
+      status: "적정",
+      statusColor: "text-green-500",
+    },
+  ];
 
-  // 중앙에 텍스트 넣는 플러그인 만들기
-  const centerTextPlugin = (text: string) => {
-    return {
-      id: "centerText",
-      beforeDraw: function (chart: unknown) {
-        const chartInstance = chart as {
-          width: number;
-          height: number;
-          ctx: CanvasRenderingContext2D;
-        };
-
-        const { width, height, ctx } = chartInstance;
-
-        ctx.restore();
-        ctx.font = "10px Arial";
-        ctx.fillStyle = "#333333";
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "center";
-        ctx.fillText(text, width / 2, height / 2);
-        ctx.save();
-      },
-    };
-  };
   return (
     <>
       <section className="mb-8 px-4">
@@ -111,105 +51,36 @@ export default function DailyReport() {
           </button>
         </div>
         <div className="mb-3 flex flex-col gap-1">
-          {/* 탄수화물 프로그레스 */}
-          <div className="bg-primary-50 rounded-lg p-4 shadow-sm">
-            <div className="flex items-center">
-              <div className="relative mr-4 h-20 w-20">
-                <Doughnut
-                  data={carbData}
-                  options={chartOptions}
-                  plugins={[centerTextPlugin("탄수화물")]}
-                />
-              </div>
-              <div className="flex-1">
-                <div className="grid grid-cols-4 gap-2 text-center text-xs">
-                  <div>목표</div>
-                  <div>현재</div>
-                  <div>+/-</div>
-                  <div>상태</div>
-                  <div className="font-medium">100g</div>
-                  <div className="font-medium">120g</div>
-                  <div className="font-medium">+20g</div>
-                  <div className="font-medium text-yellow-500">과다</div>
+          {nutritionData.map((item, index) => (
+            <div key={index} className="bg-primary-50 rounded-lg p-4 shadow-sm">
+              <div className="flex items-center">
+                <div className="relative mr-4 h-20 w-20">
+                  <DonutChart name={item.name} currentValue={item.value} totalValue={item.total} />
+                </div>
+                <div className="flex-1">
+                  <div className="grid grid-cols-4 gap-2 text-center text-xs">
+                    <div>목표</div>
+                    <div>현재</div>
+                    <div>+/-</div>
+                    <div>상태</div>
+                    <div className="font-medium">
+                      {item.name === "총 열량" ? `${item.total}kcal` : `${item.total}g`}
+                    </div>
+                    <div className="font-medium">
+                      {item.name === "총 열량" ? `${item.value}kcal` : `${item.value}g`}
+                    </div>
+                    <div className="font-medium">
+                      {item.value > item.total
+                        ? `+${item.value - item.total}`
+                        : `${item.value - item.total}`}
+                      {item.name === "총 열량" ? `kcal` : `g`}
+                    </div>
+                    <div className={`font-medium ${item.statusColor}`}>{item.status}</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          {/* 단백질 프로그레스 */}
-          <div className="bg-primary-50 rounded-lg p-4 shadow-sm">
-            <div className="flex items-center">
-              <div className="relative mr-4 h-20 w-20">
-                <Doughnut
-                  data={proteinData}
-                  options={chartOptions}
-                  plugins={[centerTextPlugin("단백질")]}
-                />
-              </div>
-              <div className="flex-1">
-                <div className="grid grid-cols-4 gap-2 text-center text-xs">
-                  <div>목표</div>
-                  <div>현재</div>
-                  <div>+/-</div>
-                  <div>상태</div>
-                  <div className="font-medium">60g</div>
-                  <div className="font-medium">22g</div>
-                  <div className="font-medium">-38g</div>
-                  <div className="font-medium text-red-500">매우 부족</div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* 지방 프로그레스 */}
-          <div className="bg-primary-50 rounded-lg p-4 shadow-sm">
-            <div className="flex items-center">
-              <div className="relative mr-4 h-20 w-20">
-                <Doughnut
-                  data={fatData}
-                  options={chartOptions}
-                  plugins={[centerTextPlugin("지방")]}
-                />
-              </div>
-              <div className="flex-1">
-                <div className="grid grid-cols-4 gap-2 text-center text-xs">
-                  <div>목표</div>
-                  <div>현재</div>
-                  <div>+/-</div>
-                  <div>상태</div>
-                  <div className="font-medium">80g</div>
-                  <div className="font-medium">74g</div>
-                  <div className="font-medium">-6g</div>
-                  <div className="font-medium text-green-500">적정</div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* 총 열량 프로그레스 */}
-          <div className="bg-primary-50 rounded-lg p-4 shadow-sm">
-            <div className="flex items-center">
-              <div className="relative mr-4 h-20 w-20">
-                <Doughnut
-                  data={calorieData}
-                  options={chartOptions}
-                  plugins={[centerTextPlugin("총 열량")]}
-                />
-              </div>
-              <div className="flex-1">
-                <div className="grid grid-cols-4 gap-2 text-center text-xs">
-                  <div>목표</div>
-                  <div>현재</div>
-                  <div>+/-</div>
-                  <div>상태</div>
-                  <div className="font-medium">2200kcal</div>
-                  <div className="font-medium">2300kcal</div>
-                  <div className="font-medium">+100kcal</div>
-                  <div className="font-medium text-green-500">적정</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
         <ReportSummary />
       </section>
